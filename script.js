b09be03350f57a4d977fd568bc6aa04bd3fa2a4f
giorgigongladze <giorgi.gongladze.1@btu.edu.ge>
2026-01-21 15:46:29 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const notesContainer = document.querySelector('.notes-container');
+const createBtn = document.querySelector('.btn');
+let notes = document.querySelectorAll('.input-box');
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem('notes');
+}
+
+function updateStorage() {
+    localStorage.setItem('notes', notesContainer.innerHTML);
+}
+showNotes();
+
+createBtn.addEventListener('click', () => {
+    const inputBox = document.createElement('p');
+    const img = document.createElement('img');
+    inputBox.className = 'input-box';
+    inputBox.contentEditable = 'true';
+    img.src = 'images/delete.png';
+    inputBox.appendChild(img);
+    notesContainer.appendChild(inputBox);
+    updateStorage();
+});
+
+notesContainer.addEventListener('click', (e) => {
+    if (e.target.tagName === 'IMG') {
+        e.target.parentElement.remove();
+        updateStorage();
+    } else if (e.target.tagName === 'P') {
+        notes = document.querySelectorAll('.input-box');
+        notes.forEach((nt) => {
+            nt.onkeyup = () => updateStorage();
+        });
+    }
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        document.execCommand('insertLineBreak');
+        event.preventDefault();
+    }
+});
